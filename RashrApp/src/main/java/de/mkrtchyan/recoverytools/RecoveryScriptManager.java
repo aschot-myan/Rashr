ed386bbc4f56d8eba3794f644e46dbdf5cf0e43c
Ashot Mkrtchyan <ashotmkrtchyan1995@gmail.com>
2014-07-28 14:18:35 +0200

    -Copyright Updated

--- RashrApp/src/main/java/de/mkrtchyan/recoverytools/RecoveryScriptManager.java
+++ RashrApp/src/main/java/de/mkrtchyan/recoverytools/RecoveryScriptManager.java
@@ -1,5 +1,26 @@
 package de.mkrtchyan.recoverytools;
 
+/**
+ * Copyright (c) 2014 Ashot Mkrtchyan
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -23,16 +44,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.TimeoutException;
 
+import de.mkrtchyan.utils.Common;
 import de.mkrtchyan.utils.FileChooserDialog;
 
 public class RecoveryScriptManager extends ActionBarActivity {
 
     final Context mContext = this;
-    private final String CMD_END = ";";
+    static private final String CMD_END = ";";
     ArrayList<File> mFileList;
     ArrayAdapter<String> mFileNameAdapter;
     ListView Queue;
     FileChooserDialog fileChooser;
+	String[] AllowedEXT = {".zip"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +81,12 @@ public class RecoveryScriptManager extends ActionBarActivity {
         Uri path;
         if ((path = getIntent().getData()) != null) {
             File zip = new File(path.getPath());
-            addFileToQueue(zip);
+	        if (Common.stringEndsWithArray(zip.getName(), AllowedEXT)) {
+		        addFileToQueue(zip);
+	        } else {
+		        Toast.makeText(mContext, R.string.wrong_format, Toast.LENGTH_SHORT).show();
+	        }
         }
-
     }
 
     public void addZip(View view) {
