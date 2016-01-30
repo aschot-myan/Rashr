d0d3049283bb2cc9fb66d581d6b54649dc846c95
Aschot Mkrtchyan <ashotmkrtchyan1995@gmail.com>
2016-01-30 17:29:55 +0100

    -Now you can get more information about your device (count of available images, Flash information, Partition information....)

---
+++ RashrApp/src/main/java/de/mkrtchyan/recoverytools/InformationFragment.java
@@ -0,0 +1,168 @@
+package de.mkrtchyan.recoverytools;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class InformationFragment extends Fragment {
+
+    //private OnListFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public InformationFragment() {
+    }
+
+    @SuppressWarnings("unused")
+    public static InformationFragment newInstance() {
+        return new InformationFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_information_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            final Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            ArrayList<InformationItem> items = new ArrayList<>();
+            items.add(new InformationItem("Device Name", RashrApp.DEVICE.getName()));
+            items.add(new InformationItem("Manufacture", RashrApp.DEVICE.getManufacture()));
+            String recoveryType = "";
+            boolean recoveryWithPath = false;
+            boolean recoveryWithBS = false;
+            switch (RashrApp.DEVICE.getRecoveryType()) {
+                case Device.PARTITION_TYPE_DD:
+                    recoveryType = "DD";
+                    recoveryWithPath = true;
+                    recoveryWithBS = RashrApp.DEVICE.getRecoveryBlocksize() > 0;
+                    break;
+                case Device.PARTITION_TYPE_MTD:
+                    recoveryType = "MTD";
+                    break;
+                case Device.PARTITION_TYPE_RECOVERY:
+                    recoveryType = "Flash over current Recovery";
+                    break;
+                case Device.PARTITION_TYPE_NOT_SUPPORTED:
+                    recoveryType = "Not supported";
+                    break;
+            }
+
+            String kernelType = "";
+            boolean kernelWithPath = false;
+            boolean kernelWithBS = false;
+            switch (RashrApp.DEVICE.getKernelType()) {
+                case Device.PARTITION_TYPE_DD:
+                    kernelType = "DD";
+                    kernelWithPath = true;
+                    kernelWithBS = RashrApp.DEVICE.getKernelBlocksize() > 0;
+                    break;
+                case Device.PARTITION_TYPE_MTD:
+                    kernelType = "MTD";
+                    break;
+                case Device.PARTITION_TYPE_RECOVERY:
+                    kernelType = "Flash over current Recovery";
+                    break;
+                case Device.PARTITION_TYPE_NOT_SUPPORTED:
+                    kernelType = "Not supported";
+            }
+
+            items.add(new InformationItem("Recovery Partition Type", recoveryType));
+            if (recoveryWithPath)
+                items.add(new InformationItem("Recovery Path", RashrApp.DEVICE.getRecoveryPath()));
+            if (recoveryWithBS)
+                items.add(new InformationItem("Recovery Blocksize", String.valueOf(RashrApp.DEVICE.getRecoveryBlocksize())));
+
+            items.add(new InformationItem("Kernel Partition Type", kernelType));
+            if (kernelWithPath)
+                items.add(new InformationItem("Kernel Path", RashrApp.DEVICE.getKernelPath()));
+            if (kernelWithBS)
+                items.add(new InformationItem("Kernel Blocksize", String.valueOf(RashrApp.DEVICE.getKernelBlocksize())));
+
+            int total = 0;
+            if (RashrApp.DEVICE.isCwmRecoverySupported()) {
+                int tmp = RashrApp.DEVICE.getCwmRecoveryVersions().size();
+                items.add(new InformationItem("CWM Images", String.valueOf(tmp)));
+                total += tmp;
+            }
+            if (RashrApp.DEVICE.isTwrpRecoverySupported()) {
+                int tmp = RashrApp.DEVICE.getTwrpRecoveryVersions().size();
+                items.add(new InformationItem("TWRP Images", String.valueOf(tmp)));
+                total += tmp;
+            }
+            if (RashrApp.DEVICE.isPhilzRecoverySupported()){
+                int tmp = RashrApp.DEVICE.getPhilzRecoveryVersions().size();
+                items.add(new InformationItem("Philz Images", String.valueOf(tmp)));
+                total += tmp;
+            }
+            if (RashrApp.DEVICE.isXZDualRecoverySupported()) {
+                int tmp = RashrApp.DEVICE.getXZDualRecoveryVersions().size();
+                items.add(new InformationItem("XZDual Images", String.valueOf(tmp)));
+                total += tmp;
+            }
+            if (RashrApp.DEVICE.isStockRecoverySupported()){
+                int tmp = RashrApp.DEVICE.getStockRecoveryVersions().size();
+                items.add(new InformationItem("Stock Recovery Images", String.valueOf(tmp)));
+                total += tmp;
+            }
+            if (RashrApp.DEVICE.isStockKernelSupported()){
+                int tmp = RashrApp.DEVICE.getStockKernelVersions().size();
+                items.add(new InformationItem("Stock Kernel Images", String.valueOf(tmp)));
+                total += tmp;
+            }
+
+            items.add(new InformationItem("Total images avaiable", String.valueOf(total)));
+
+            for (String err : RashrApp.ERRORS) {
+                items.add(new InformationItem("Err", err));
+            }
+
+            recyclerView.setAdapter(new InformationRecyclerViewAdapter(items));
+        }
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        //mListener = new OnListFragmentInteractionListener() {
+        //    @Override
+        //    public void onListFragmentInteraction(InformationItem item) {
+//
+        //    }
+        //};
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        //mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    //public interface OnListFragmentInteractionListener {
+    //    void onListFragmentInteraction(InformationItem item);
+    //}
+}
---
+++ RashrApp/src/main/java/de/mkrtchyan/recoverytools/InformationItem.java
@@ -0,0 +1,21 @@
+package de.mkrtchyan.recoverytools;
+
+
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public class InformationItem {
+        public final String id;
+        public final String content;
+
+        public InformationItem(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
