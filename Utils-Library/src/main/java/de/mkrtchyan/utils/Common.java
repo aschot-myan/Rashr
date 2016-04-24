012e9cb1d07ba8fc465f507c7f40bbb27b7ea928
Aschot Mkrtchyan <ashotmkrtchyan1995@gmail.com>
2016-04-24 21:13:41 +0200

    -Now appending "new line character" while reading file to string

--- Utils-Library/src/main/java/de/mkrtchyan/utils/Common.java
+++ Utils-Library/src/main/java/de/mkrtchyan/utils/Common.java
@@ -27,7 +27,6 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -142,7 +141,7 @@ public class Common {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line;
         while ((line = reader.readLine()) != null) {
-            content += line;
+            content += line + "\n";
         }
         return content;
     }
